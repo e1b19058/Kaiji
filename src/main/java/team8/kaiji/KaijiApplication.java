7c9b262d30ffe5ce4d8a93bc3a928c3fef00d7b6
e1b19058 <e1b19058@oit.ac.jp>
2021-11-09 15:22:58 +0900

    初期化

---
+++ src/main/java/team8/kaiji/KaijiApplication.java
@@ -0,0 +1,13 @@
+package team8.kaiji;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaijiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KaijiApplication.class, args);
+	}
+
+}
