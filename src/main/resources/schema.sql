4db16a5c739274e28f3a30867609666f28e0d210
e1b19058 <e1b19058@oit.ac.jp>
2021-11-30 15:34:29 +0900

    makematchroom途中

--- src/main/resources/schema.sql
+++ src/main/resources/schema.sql
@@ -6,3 +6,11 @@ CREATE TABLE users(
   pa int not null,
   star int not null
 );
+
+CREATE TABLE match(
+  matchid IDENTITY,
+  user1id int not null,
+  user2id int not null,
+  user1hand char not null,
+  user2hand char
+);
